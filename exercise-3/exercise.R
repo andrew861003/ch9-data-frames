31d88287f4bd8b153e235935f465c5cb9d457cfa
Andrew <syw1003@uw.edu>
2018-04-11 00:00:38 -0700

    exercise completed

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,43 +2,46 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+pe <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(pe)
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(pe)
 
 # Add a column "category" to your data frame that contains the rownames
-
+pe$category <- rownames(pe)
 
 # How much money was spent on personal care in 1940?
-
+pe['Personal Care','X1940']
 
 # How much money was spent on Food and Tobacco in 1960?
-
+pe['Food and Tobacco','X1960']
 
 # What was the highest expenditure category in 1960?
-
+pe[pe$X1960==max(pe$X1960), 'category']
 
 # Define a function `lowest_category` that takes in a year as a parameter, and
 # returns the lowest spending category of that year
-
+lowest_category <- function(year){
+  a <- paste("X", year, sep = "")
+  pe[pe[, a]==min(pe[, a]), 'category']
+}
 
 # Using your function, determine the lowest spending category of each year
 # Hint: use the `sapply()` function to apply your function to a vector of years
-
+sapply(seq(from = 1940, to = 1960, by = 5), lowest_category)
